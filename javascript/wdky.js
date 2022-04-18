0a6261902ae938c0b874e58d5eabe5ae88537c53
maryrochon <99832210+maryrochon@users.noreply.github.com>
2022-04-18 11:34:37 -0400

    Add files via upload

---
+++ javascript/wdky.js
@@ -0,0 +1,81 @@
+$("#wdky1").hover(function(){
+    $(".graph").toggleClass("wd1")
+if($("#wdky1:hover").length == 1) {
+    $("#red1").css("opacity", "1")
+    }
+ else{
+    $("#red1").css("opacity", "0")
+ }
+})
+$("#wdky2").hover(function(){
+    $(".graph").toggleClass("wd2")
+    if($("#wdky2:hover").length == 1) {
+    $("#red2").css("opacity", "1")
+    }
+ else{
+    $("#red2").css("opacity", "0")
+ }
+})
+$("#wdky3").hover(function(){
+    $(".graph").toggleClass("wd3")
+    if($("#wdky3:hover").length == 1) {
+    $("#red3").css("opacity", "1")
+    }
+ else{
+    $("#red3").css("opacity", "0")
+ }
+})
+$("#wdky4").hover(function(){
+    $(".graph").toggleClass("wd4")
+    if($("#wdky4:hover").length == 1) {
+    $("#red4").css("opacity", "1")
+    }
+ else{
+    $("#red4").css("opacity", "0")
+ }
+})
+$("#wdky5").hover(function(){
+    $(".graph").toggleClass("wd5")
+    if($("#wdky5:hover").length == 1) {
+    $("#red5").css("opacity", "1")
+    }
+ else{
+    $("#red5").css("opacity", "0")
+ }
+})
+$("#wdky6").hover(function(){
+    $(".graph").toggleClass("wd6")
+    if($("#wdky6:hover").length == 1) {
+    $("#red6").css("opacity", "1")
+    }
+ else{
+    $("#red6").css("opacity", "0")
+ }
+})
+$("#wdky7").hover(function(){
+    $(".graph").toggleClass("wd7")
+    if($("#wdky7:hover").length == 1) {
+    $("#red7").css("opacity", "1")
+    }
+ else{
+    $("#red7").css("opacity", "0")
+ }
+})
+$("#wdky8").hover(function(){
+    $(".graph").toggleClass("wd8")
+    if($("#wdky8:hover").length == 1) {
+    $("#red8").css("opacity", "1")
+    }
+ else{
+    $("#red8").css("opacity", "0")
+ }
+})
+$("#wdky9").hover(function(){
+    $(".graph").toggleClass("wd9")
+    if($("#wdky9:hover").length == 1) {
+    $("#red9").css("opacity", "1")
+    }
+ else{
+    $("#red9").css("opacity", "0")
+ }
+})
